d05ea25ac0def3f1dca83aca8398665fc5daa5f3
sergs <ivan0ivanov0@mail.ru>
2016-06-27 19:51:50 +0300

    improve spidev_test.

--- spidev_test/load.sh
+++ spidev_test/load.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+odroid_ip=10.42.0.116
+
 app_name=spidev_test
-app_sources="${app_name}.c n_rf24l01.c"
+app_sources="${app_name}.c init_n_rf24l01.c ../src/n_rf24l01_lib/src/n_rf24l01.c"
 
 echo "reload ${app_name} app and it sources."
 
 echo -n "remove ${app_name} directory..."
-ssh odroid@192.168.150.8 "rm -rf ${app_name}" && echo "ok." || echo "fail." 
+ssh odroid@${odroid_ip} "rm -rf ${app_name}" && echo "ok." || echo "fail." 
 
 echo -n "create ${app_name} directory..."
-ssh odroid@192.168.150.8 "mkdir ${app_name}" && echo "ok." || echo "fail." 
+ssh odroid@${odroid_ip} "mkdir ${app_name}" && echo "ok." || echo "fail." 
 
 echo -n "copy ${app_name} to odroid..."
-scp ${app_name} odroid@192.168.150.8:/home/odroid/${app_name} && echo "ok." || echo "fail." 
+scp ${app_name} odroid@${odroid_ip}:/home/odroid/${app_name} && echo "ok." || echo "fail." 
 
-# copy app's sources to use them in gdb
+# copy app's sources to use them with gdb
 echo -n "copy ${app_sources} to odroid..."
-scp ${app_sources} odroid@192.168.150.8:/home/odroid/${app_name} && echo "ok." || echo "fail." 
+scp ${app_sources} odroid@${odroid_ip}:/home/odroid/${app_name} && echo "ok." || echo "fail." 
--- spidev_test/spidev_test.c
+++ spidev_test/spidev_test.c
@@ -12,110 +12,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "n_rf24l01.h"
+#include <unistd.h>
+
+#include "init_n_rf24l01.h"
 
 //
 //======================================================================================================
 int main ( void )
 {
-	int ret;
-	char str[] = "Lena I want to make your life more happy.";
-	char* byte;
-	__u8 reg;
-	__u64 data = 0;
+  char str[] = "Lena I want to make your life more happy.\n";
+  int ret;
 
-	byte = str;
+  ret = init_n_rf24l01();
+  if( ret < 0 )
+    return 1;
 
-	ret = init_n_rf24l01();
-	if( ret < 0 ) return 1;
+  while( 1 )
+  {
+    printf( "transmit: %s\n", str );
+    transmit_str( str );
 
-	prepare_to_transmit();
+    //sleep( 2 );
+  }
 
-    while( 1 )
-    {
-      printf( "transmit %c.\n", *byte );
-
-      data = (__u64)*byte;
-      send_command( W_TX_PAYLOAD, NULL, &data, 1, 1 );
-
-      // CE up... sleep 10 us... CE down - to actual data transmit (in space)
-      set_up_ce_pin( 1 );
-      usleep( 10 );
-      set_up_ce_pin( 0 );
-
-      // round-robin buffer
-      if( *(++byte) == 0 )
-      {
-        byte = str;
-      }
-
-      wait_pkg_transmitted();
-
-      sleep( 1 );
-    }
-
-	read_register( CONFIG_RG, &reg );
-	read_register( STATUS_RG, &reg );
-
-	send_command( W_TX_PAYLOAD, NULL, (__u64*)"9", 1, 1 );
-
-
-
-	read_register( STATUS_RG, &reg );
-	clear_pending_interrupts();
-	read_register( STATUS_RG, &reg );
-
-
-	prepare_to_receive();
-
-	while(1)
-	{
-		read_register( STATUS_RG, &reg );
-		if( reg != 0x0e )
-		{
-			send_command( R_RX_PAYLOAD, NULL, &data, 1, 0 );
-			clear_pending_interrupts();
-		}
-	}
-
-	ret = prepare_to_transmit();
-	if( ret < 0 ) return 1;
-
-	read_register( CONFIG_RG, &reg );
-	printf( "before while(1): CONFIG_RG: 0x%02hhx.\n", reg );
-
-	read_register( STATUS_RG, &reg );
-	printf( "before while(1): STATUS_RG: 0x%02hhx.\n", reg );
-
-	set_up_ce_pin( 1 );
-	usleep( 10 );
-	//set_up_ce_pin( 0 );
-
-	while( 1 )
-	{
-		send_command( W_TX_PAYLOAD, NULL, (__u64*)byte++, 1, 1 );
-
-		printf( "byte %c has been transmitted to space.\n", *(byte - 1) );
-
-		// round-robin buffer to transmit
-		if( *byte == '\0' )
-			byte = str;
-
-		sleep(1);
-	}
-
-	// we transmit one byte one time in second
-	while( 1 )
-	{
-		ret = transmit_byte( *byte++ );
-		if( ret < 0) return 0;
-
-		printf( "byte %c has been transmitted to space.\n", *(byte - 1) );
-
-		// round-robin buffer to transmit
-		if( *byte == '\0' )
-			byte = str;
-	}
-
-	return 0;
+  return 0;
 }
